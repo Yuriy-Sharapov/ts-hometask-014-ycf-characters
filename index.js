d781f1684e5affbd8ee3e320078282c7761b9b2a
Yuriy <j.a.sharapov@gmail.com>
2024-10-30 19:55:05 +0500

    Получение героев через одну ручку /api/characters

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-import serverless from 'serverless-http'
+const serverless = require('serverless-http')
 const charactersDb = require('./characters_db.json');
 
 const app = express()
@@ -7,22 +7,34 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.get('/api/characters', (req, res) => {
-    res.json(charactersDb)
+
+    const { id } = req.query
+
+    // Если id не задан, то выводим весь список
+    if (!id) res.json(charactersDb)
+    else {
+        // Если id задан, то ищем конкретный элемент списка
+        const character = charactersDb.find((char) => char.id == id )
+        if (character)
+            res.json(character) 
+        else
+            res.json('Code: 404')
+    }
 })
 
-app.get('/api/characters/:id', (req, res) => {
-    const { id } = req.params
+// app.get('/api/characters/:id', (req, res) => {
+//     const { id } = req.params
 
-    const character = charactersDb.find((char) => char.id == id )
-    if (character)
-        res.json(character) 
-    else
-        res.json('Code: 404')
-})
+//     const character = charactersDb.find((char) => char.id == id )
+//     if (character)
+//         res.json(character) 
+//     else
+//         res.json('Code: 404')
+// })
 
 // const PORT = process.env.PORT || 3000
 // app.listen(PORT, () =>{
 //     console.log(`Server is listening a PORT ${PORT}`)  
-// })     
+// })
    
 module.exports.handler = serverless(app)  
